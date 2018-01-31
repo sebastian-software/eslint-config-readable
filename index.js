7747e10b740d9f3a17b5ffce9cc1f49729e4c176
Sebastian Werner <s.werner@sebastian-software.de>
2018-01-31 17:25:29 +0100

    Disabling mutation checks for pure NodeJS libs/entry points.

--- index.js
+++ index.js
@@ -98,7 +98,12 @@ module.exports = {
       rules: {
         // This is super useful for browser development as it uses `browserslist`.
         // Unfortunately this does not offer any support for NodeJS.
-        "compat/compat": "off"
+        "compat/compat": "off",
+
+        // Immutability is mainly interesting for client side
+        // state management like Redux. Not so much required
+        // when doing pure NodeJS libraries and cli tools.
+        "immutable/no-mutation": "off"
       }
     }
   ]
