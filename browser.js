89ea2b2d63f1e4473ed2125113a4bb4dbba85ec6
Sebastian Werner <s.werner@sebastian-software.de>
2018-06-11 17:50:47 +0200

    Moved 'fetch' to be browser only. In NodeJS it's typically imported, but not polyfilled.

--- browser.js
+++ browser.js
@@ -3,6 +3,10 @@ module.exports = {
   extends: [
     "./index.js"
   ],
+  globals: {
+    // Globally allow fetch which is generally polyfilled if not available natively.
+    fetch: false
+  },
   env: {
     browser: true,
     node: false
